c21fdf21afa09fcf23b2d061c91296366f21d3a8
Abdeselam El-Haman Abdeselam <aelhaman@gmail.com>
2017-12-06 05:06:44 +0100

    First additions in Treap classe

--- homework-treap/Treap.py
+++ homework-treap/Treap.py
@@ -1,2 +1,15 @@
 class Treap:
-    pass
+    def __init__(self):
+        self.root = None
+
+    def insert(self,value):
+        pass
+
+    def delete(self,value):
+        pass
+
+    def find(self,value):
+        """
+            Finds node that contains value and returns it, used in INSERT and DELETE.
+        """
+        pass
--- homework-treap/TreapNode.py
+++ homework-treap/TreapNode.py
@@ -1,2 +1,37 @@
 class TreapNode:
-    pass
+    """
+        Key = BST value
+        Value = Heap value
+    """
+    def __init__(self,key,value,left=None,right=None,parent=None):
+
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+        self.key = key
+        self.value = value
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+    def getParent(self):
+        return self.parent
+
+    def setLeft(self,new):
+        self.left = new
+
+    def setRight(self):
+        self.right = new
+
+    def setParent(self):
+        self.parent = new
+
+    def getKey(self):
+        return self.key
+
+    def getValue(self):
+        return self.value
